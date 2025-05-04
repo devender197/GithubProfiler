2f03f52fed131e6886aebed8bb9e03bc74683b55
dsingh <devender197@gmail.com>
2025-05-03 22:35:51 -0700

    Initial Commit

---
+++ GithubProfiler/GithubProfilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubProfilerApp.swift
+//  GithubProfiler
+//
+//  Created by Devender Singh on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubProfilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
